fc17b7bc3f6d3d6d44065c0754f534a30585babd
Stephen Ni <stephenni1234@gmail.com>
2022-06-29 12:52:51 -0400

    lol

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    <h3 className="">LOL</h3>
   );
 }
 
---
+++ src/components/PageLayout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const PageLayout = () => {
+  return (
+    <div>
+        <Navbar />
+        <div className="text-orange-900"> </div>
+    </div>
+  )
+}
+
+export default PageLayout
